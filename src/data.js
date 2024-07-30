44a16dac4b3a4ad91ce1f0f562522bb3267e7e0b
zahidhasanzh <zahidhasanzh000@gmail.com>
2024-07-30 19:29:32 +0600

    cart page setup

---
+++ src/data.js
@@ -0,0 +1,32 @@
+let shopItemsData = [
+    {
+        id: "sdfskdkf",
+        name: "Casual Shirt",
+        price: 45,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+        img: "images/img-1.jpg"
+    },
+    
+    {
+        id: "kjksasdf",
+        name: "Office Shirt",
+        price: 100,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+        img: "images/img-2.jpg"
+    },
+    {
+        id: "dfskeom",
+        name: "T Shirt",
+        price: 25,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+        img: "images/img-3.jpg"
+    },
+    
+    {
+        id: "jkjoasdk",
+        name: "Mens Suit",
+        price: 300,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+        img: "images/img-4.jpg"
+    }
+]
